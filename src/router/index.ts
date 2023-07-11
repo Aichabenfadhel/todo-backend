a8785d206ceb59160c41ea542bed4a77d410690a
aicha <aichaaichabff@gmail.com>
2023-07-11 17:20:16 +0000

    fix: fixing `API and routes`in `router`

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,38 +1,44 @@
-import { Application } from "express";
+import { Application, Request, Response } from "express";
 import path from "path";
+
 import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUI from "swagger-ui-express";
-import cors from "cors";
+import { TodoRouter } from "./todoRouter";
 import usersRouter from "./users/usersRouter";
 
-export default (app: Application) => {
-  const options = {
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "first API",
-        version: "1.0.0",
-        description:
-          "This page is dedicated to the route list in the application",
-      },
-      servers: [
-        {
-          url: "http://localhost:" + process.env.PORT + "/api/v1",
+export default (app: Application): void => {
+    const options = {
+        definition: {
+            openapi: "3.0.0",
+            info: {
+                title: "MY API",
+                version: "1.0.0",
+                description:
+                    "This page is dedicated to the route list in the application",
+            },
+            servers: [
+                {
+                    url: "http://localhost:7000/api_ToDo/v1",
+                },
+            ],
         },
-      ],
-    },
-    apis: [
-      `${__dirname}/*${path.extname(path.basename(__filename))}`,
-      `${__dirname}/*/*${path.extname(path.basename(__filename))}`,
-      `${__dirname}/*/*/*${path.extname(path.basename(__filename))}`,
-    ],
-  };
-  const specs = swaggerJsDoc(options);
+        apis: [
+            `${__dirname}/*${path.extname(path.basename(__filename))}`,
+            `${__dirname}/*/*${path.extname(path.basename(__filename))}`,
+            `${__dirname}/*/*/*${path.extname(path.basename(__filename))}`,
 
-  app.use("/api/v1/docs", swaggerUI.serve, swaggerUI.setup(specs));
-  app.get("/", (req, res) => {
-    res.json({ message: "API Running ! " });
-  });
+            `${__dirname}/*.yml`,
+            `${__dirname}/*/*.yml`,
+            `${__dirname}/*/*/*.yml`,
+        ],
+    };
+    const specs = swaggerJsDoc(options);
 
-  app.use("/api/v1/", [usersRouter]);
+    app.use("/api_ToDo/v1/docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+    app.get("/", (_: Request, response: Response) => {
+        response.json({ message: "API Running ! " });
+    });
+
+    app.use("/api_ToDo/v1", [TodoRouter, usersRouter]);
 };
