ca05c7d13e6c8dfb0926dd00840d78f4653cffe1
aicha <aichaaichabff@gmail.com>
2023-07-12 22:25:31 +0000

    fix:fixing routes in `router/Todo`

--- src/router/todoRouter/index.ts
+++ src/router/todoRouter/index.ts
@@ -9,9 +9,9 @@ import {
 
 export const TodoRouter = express.Router();
 
-TodoRouter.route("/todoById/:todo_id").get(getTodoByIdController);
+TodoRouter.route("/todoById/:email/:pwd/:id").get(getTodoByIdController);
 
-TodoRouter.route("/allTodos").get(getAllTodosController);
-TodoRouter.route("/addtodo").post(addTodoController);
-TodoRouter.route("/updateTodo/:todo_id").put(updateTodoController);
+TodoRouter.route("/allTodos/:email/:pwd").get(getAllTodosController);
+TodoRouter.route("/addtodo/:email/:pwd").post(addTodoController);
+TodoRouter.route("/updateTodo/:id").put(updateTodoController);
 TodoRouter.route("/deleteTodo/:todo_id").delete(deleteTodoController);
