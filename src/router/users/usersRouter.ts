4fd1c5fb6978d1be003f0e897f64d9037098af7e
aicha <aichaaichabff@gmail.com>
2023-07-12 10:19:44 +0000

    chore: delete `login` route in usersRouter

--- src/router/users/usersRouter.ts
+++ src/router/users/usersRouter.ts
@@ -4,7 +4,6 @@ import {
     addUserController,
     getUsersController,
     deleteUserController,
-    // login,
     userLoginController,
 } from "../../controllers/users/userController";
 
@@ -13,6 +12,5 @@ const usersRouter = express.Router();
 usersRouter.route("/delete").delete(deleteUserController);
 usersRouter.route("/adduser").post(addUserController);
 usersRouter.route("/users").get(getUsersController);
-// usersRouter.route("/login").post(login);
 usersRouter.route("/getUser/:email/:pwd").get(userLoginController);
 export default usersRouter;
