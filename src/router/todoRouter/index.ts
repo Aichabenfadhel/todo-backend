e3c7aefdf1c28cc0242f91ce7b477bd529d2a0ba
aicha <aichaaichabff@gmail.com>
2023-07-12 23:55:58 +0000

    fix: fixing route of `deleteTodoController` in `route/todo`

--- src/router/todoRouter/index.ts
+++ src/router/todoRouter/index.ts
@@ -14,4 +14,4 @@ TodoRouter.route("/todoById/:email/:pwd/:id").get(getTodoByIdController);
 TodoRouter.route("/allTodos/:email/:pwd").get(getAllTodosController);
 TodoRouter.route("/addtodo/:email/:pwd").post(addTodoController);
 TodoRouter.route("/updateTodo/:email/:pwd/:id").put(updateTodoController);
-TodoRouter.route("/deleteTodo/:todo_id").delete(deleteTodoController);
+TodoRouter.route("/deleteTodo/:email/:pwd/:id").delete(deleteTodoController);
