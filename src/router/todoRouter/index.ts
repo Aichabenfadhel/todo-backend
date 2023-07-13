e3ef437a24da92cef222fa74a496a990b88fdb89
aicha <aichaaichabff@gmail.com>
2023-07-13 09:29:14 +0000

    feat: add `updateTodocompletedController` route in `route/todo`

--- src/router/todoRouter/index.ts
+++ src/router/todoRouter/index.ts
@@ -4,6 +4,7 @@ import {
     deleteTodoController,
     getAllTodosController,
     getTodoByIdController,
+    updateTodocompletedController,
     updateTodoController,
 } from "../../controllers/Todo";
 
@@ -14,4 +15,7 @@ TodoRouter.route("/todoById/:email/:pwd/:id").get(getTodoByIdController);
 TodoRouter.route("/allTodos/:email/:pwd").get(getAllTodosController);
 TodoRouter.route("/addtodo/:email/:pwd").post(addTodoController);
 TodoRouter.route("/updateTodo/:email/:pwd/:id").put(updateTodoController);
+TodoRouter.route("/updateTodocompleted/:email/:pwd/:id").put(
+    updateTodocompletedController
+);
 TodoRouter.route("/deleteTodo/:email/:pwd/:id").delete(deleteTodoController);
